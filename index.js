0444153e23e5d276f4be988e8e6d99c390e57d0d
Harscoet Donovan <donovan.harscoet@gmail.com>
2016-08-15 18:42:10 +0200

    Flags

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 var childProcess = require('child_process');
 var gutil = require('gulp-util');
 var through = require('through2');
+var toSpawnArgs = require('object-to-spawn-args');
 
 var BIN = require.resolve('ava/cli.js');
 
-module.exports = function () {
+module.exports = function (flags) {
 	var files = [];
 
 	return through.obj(function (file, enc, cb) {
@@ -25,7 +26,7 @@ module.exports = function () {
 	}, function (cb) {
 		files.unshift(BIN);
 
-		childProcess.execFile(process.execPath, files.concat('--color'), function (err, stdout, stderr) {
+		childProcess.execFile(process.execPath, files.concat('--color', toSpawnArgs(flags)), function (err, stdout, stderr) {
 			if (err) {
 				this.emit('error', new gutil.PluginError('gulp-ava', stderr || stdout || err));
 				return;
