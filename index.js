1450e6d2d8481e58ee3252d6073ecccb718ce36c
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2016-08-30 15:45:59 +0200

    Also execute callback if there has been an error (#15)

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ module.exports = function (opts) {
 		childProcess.execFile(process.execPath, args, function (err, stdout, stderr) {
 			if (err) {
 				this.emit('error', new gutil.PluginError('gulp-ava', stderr || stdout || err));
+				cb();
 				return;
 			}
 
